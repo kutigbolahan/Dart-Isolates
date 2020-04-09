31761f56b501743daf068f720addf3022068dfec
kuti gbolahan <kutigbolahan@ymail.com>
2020-04-09 14:07:15 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/isolatesparsingjson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.isolatesparsingjson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/model.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+
+class Photo{
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({this.albumId, this.id, this.title, this.url, this.thumbnailUrl});
+
+
+factory Photo.fromJson(Map<String, dynamic>json){
+  return Photo(
+    albumId: json['albumId'] as int,
+    id: json['id'] as int,
+    title: json['title'] as String,
+    thumbnailUrl: json['thumbnailUrl'] as String,
+    url: json['url'] as String
+  );
+}
+}
